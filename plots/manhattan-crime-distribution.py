3cab25a06e8b91022b8ac9cf3a6f3b8e91ade4c0
Chiquita Prabhu <chiquita.prabhu@gmail.com>
2017-04-17 16:41:46 -0400

    Create manhattan-crime-distribution.py

---
+++ plots/manhattan-crime-distribution.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+crime=pd.read_csv('NYPD_Complaint_Data_Historic.csv')
+is_burrow=crime['BORO_NM']=="MANHATTAN"
+high_crime=crime[is_burrow]
+h_crime=high_crime['OFNS_DESC'].value_counts()
+h_crime.plot(kind='bar')
+plt.show()
